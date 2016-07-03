19fcad0a2a1af1cbe155c859a7394b24eccc0c87
benlayer291 <benlayer291@gmail.com>
2016-07-03 21:16:47 +0100

    Add code to settings pages

--- src/scripts/settings/settings-items.controller.js
+++ src/scripts/settings/settings-items.controller.js
@@ -1,6 +1,8 @@
 SettingsItemsCtrl.$inject = ['$rootScope', '$state'];
 
 function SettingsItemsCtrl($rootScope, $state) {
+  // Highlight any code on the page.
+  Prism.highlightAll();
  
   var self = this;
 
