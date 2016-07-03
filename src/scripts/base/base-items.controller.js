f9672ac3767a8011c1728867e759d800827a895f
benlayer291 <benlayer291@gmail.com>
2016-07-03 21:04:56 +0100

    Add code to base pages

--- src/scripts/base/base-items.controller.js
+++ src/scripts/base/base-items.controller.js
@@ -1,11 +1,12 @@
 BaseItemsCtrl.$inject = ['$rootScope', '$state'];
 
 function BaseItemsCtrl($rootScope, $state) {
- 
+  // Highlight any code on the page.
+  Prism.highlightAll();
+
   var self = this;
 
   self.pageTitle = $rootScope.pageTitle.replace('Styleguide - ', '');
-
 }
 
 module.exports = BaseItemsCtrl;
